198f0f16972664fc42fa5e42b16c5b9a10d0cae9
clarob00 <66927320+clarob00@users.noreply.github.com>
2022-09-14 22:03:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.header('Breakfast menu')
 streamlit.text('\U0001f600 Omega 3 & Blueberry Oatmeal')
 
 streamlit.header('Build your own fruit smoothie')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
