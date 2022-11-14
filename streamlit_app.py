384c93840e402b2c36768c4d31f3a9ca0a7a7b9a
clarob00 <66927320+clarob00@users.noreply.github.com>
2022-11-13 20:39:20 -0500

    Update streamlit_app.py
    filter based on picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ streamlit.text('\U0001f600 Omega 3 & Blueberry Oatmeal')
 streamlit.header('Build your own fruit smoothie')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
 
